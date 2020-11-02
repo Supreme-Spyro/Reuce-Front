0dbaba5c993537ccb3ab3325c0882851cad2d255
imamfahr <imamfahr@gmail.com>
2020-11-02 12:45:01 +0700

    styling profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,51 @@
 import React from "react";
-import { Container, Row, Col, Form, Button, ListGroup} from "react-bootstrap";
+import { Container, Row, Col, Form, Button, ListGroup } from "react-bootstrap";
+
+
+// styling
+import '../styles/Profile.css'
+
+//pic import
+import blankAva from '../assets/blank-avatar.png'
 
 function Profile() {
+    function alertClicked() {
+        alert('You clicked the third ListGroupItem');
+      }
+
+
+
+
   return (
-    <div>
-      <Container>
+    <div >
+      <Container className='profileContainer-profile'>
         <Row className="m-5">
           <Col lg={3}>
-            <Row>Pic here</Row>
-            <Row>
-              <ListGroup variant="flush">
-                <ListGroup.Item>My Profile</ListGroup.Item>
-                <ListGroup.Item>My Shop</ListGroup.Item>
-                <ListGroup.Item>My Article</ListGroup.Item>
+            <Row >
+                <img 
+                src={blankAva}
+                className="blankAva-profile"
+                />
+                </Row>
+                <Row className='rowAva-profile'>
+                <div>
+                    xxx@mail.com
+                    <br />
+                    id : xxxxxxx
+                </div>
+                </Row>
+
+            <Row >
+                <Col lg={12}>
+              <ListGroup defaultActiveKey="#link1" variant='flush' mt={5}>
+                <ListGroup.Item action onClick={alertClicked} disabled >
+                  My Profile
+                </ListGroup.Item>
+                <ListGroup.Item action onClick={alertClicked}>
+                  My Shop
+                </ListGroup.Item>
               </ListGroup>
+                </Col>
             </Row>
           </Col>
           <Col lg={9}>
@@ -33,30 +65,24 @@ function Profile() {
                 <Form.Control type="email" name="email" />
               </Form.Group>
 
-              <Form.Group controlId="currentPassword" as={Col}>
-                <Col xs={6}>
-                  <Form.Label>current password</Form.Label>
-                  <Form.Control type="password" name="currentPassword" />
-                </Col>
-              </Form.Group>
-
-              <Form.Group controlId="newPassword" as={Col}>
-                <Col xs={6}>
-                  <Form.Label>new password</Form.Label>
-                  <Form.Control type="password" name="newPassword" />
-                </Col>
-              </Form.Group>
-
-              <Form.Group controlId="confirmNewPassword" as={Col}>
-                <Col xs={6}>
-                  <Form.Label>re-type the password</Form.Label>
-                  <Form.Control type="password" name="newPassword" />
-                </Col>
-              </Form.Group>
-
               <Form.Group controlId="Address" as={Row}>
                 <Form.Label>Address</Form.Label>
                 <Form.Control type="text" name="Address" />
+              </Form.Group>
+
+              <Form.Group controlId="currentPassword" as={Row}>
+                <Form.Label>current password</Form.Label>
+                <Form.Control type="password" name="currentPassword" />
+              </Form.Group>
+
+              <Form.Group controlId="newPassword" as={Row}>
+                <Form.Label>new password</Form.Label>
+                <Form.Control type="password" name="newPassword" />
+              </Form.Group>
+
+              <Form.Group controlId="confirmNewPassword" as={Row}>
+                <Form.Label>re-type new password</Form.Label>
+                <Form.Control type="password" name="newPassword" />
               </Form.Group>
 
               <Button type="submit">Save Changes</Button>
